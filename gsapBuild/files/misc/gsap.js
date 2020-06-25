4539e3649980c5c041a562378282bf18e0309d7f
lexjonker <a.j.a.jonker@hotmail.com>
2020-06-25 09:38:34 +0200

    Update GSAP verloop

--- gsapBuild/files/misc/gsap.js
+++ gsapBuild/files/misc/gsap.js
@@ -2,48 +2,24 @@ const box1 = gsap.timeline();
 const box2 = gsap.timeline();
 const box3 = gsap.timeline();
 
-box1.from("#img1", {
-  duration: 1,
-  y: -400,
-  opacity: 0,
-  ease: "power2",
-  scale: 0,
-});
-
-box1.from("#img2", {
-  duration: 1,
-  y: -400,
-  opacity: 0,
-  ease: "power2",
-  scale: 0,
-});
-
-box1.from(
-  "#img3",
-  { duration: 1, y: -400, opacity: 0, ease: "power2", scale: 0 },
-  "-=4"
-);
-
-box1.from(
-  "#img4",
-  { duration: 1, y: -400, opacity: 0, ease: "power2", scale: 0 },
-  "-=4"
-);
-
-box1.from("#img5", { duration: 1, opacity: 0, ease: "back", scale: 0 });
+box1.from("#img1", { duration: 1, y: -400, opacity: 0, ease: "power2", scale: 0,})
+    .from("#img2", { duration: 1, y: -400, opacity: 0, ease: "power2", scale: 0,})
+    .from("#img3", { duration: 1, y: -400, opacity: 0, ease: "power2", scale: 0 })
+    .from("#img4", { duration: 1, y: -400, opacity: 0, ease: "power2", scale: 0 })
+    .from("#img5", { duration: 1, opacity: 0, ease: "back", scale: 0 });
 
 box1.to(".box1", { duration: 1, x: -550, ease: "power1", scale: 1 });
 
 // VOLGENDE
 
-box2.from("#img6", { duration: 1, y: -250, ease: "back", scale: 0 }, "+=4")
+box2.from("#img6", { duration: 1, y: -250, ease: "back", scale: 0 },"+=6")
     .from("#img7, #img8, #img9, #img10", {duration: 1, y: 250, ease: "power2", scale: 0})
     .from("#img11", {duration: 1, ease: "back.out(1.8)", scale: 0})
     .to(".box2", {duration: 1, x: 550, ease: "power1", scale: 1});
 
 // VOLGENDE
 
-box3.from("#img12", {duration: 1, ease: "back.inOut(2)", opacity: 0, scale: 0.5}, "+=8")
+box3.from("#img12", {duration: 1, ease: "back.inOut(2)", opacity: 0, scale: 0.5}, "+=10")
     .from("#img14", {duration: 1, ease: "power2.easeOut(2)", opacity:0, scale: 0})
     .from("#img15", {duration: 1, ease: "back.out(12)", opacity: 0, scale: 0})
     .from("#img16", {duration: 1, y: 700, ease: "power1.out(2)", opacity: 0, scale: 1})
